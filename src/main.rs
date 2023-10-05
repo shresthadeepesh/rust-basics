e709a36ee82079e87849c90b37daffd5cb4741cd
Dipesh Shrestha <dipeshshrestha375@gmail.com>
2023-10-05 09:34:18 +0545

    Changed socket ip.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener};
 
 use crate::models::post::Post;
 use std::error::Error;
+use std::net::SocketAddr;
 use std::{fs, io};
 
 pub mod models;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(addr).await?;
     println!("Server listeneing on port: 8080");
 
     loop {
